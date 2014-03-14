c382257c1cdb615442b4ee1123df19a4727ebdee
David San <davidsanfr@gmail.com>
2014-03-14 02:01:47 +0100

    Ajout des accidents (via générateur d'événement de type accident)

---
+++ input/AccidentEvent.java
@@ -0,0 +1,37 @@
+package input;
+
+import java.util.Random;
+
+import movement.map.MapNode;
+import movement.map.SimMap;
+import core.SimClock;
+import core.SimScenario;
+import core.World;
+
+public class AccidentEvent extends ExternalEvent {
+
+	private static final long serialVersionUID = 1L;
+
+	public AccidentEvent(double time) {
+		super(time);
+	}
+
+	@Override
+	public void processEvent(World world) {
+		SimMap map = SimScenario.getInstance().getMap();
+		if (map == null) {
+			return;
+		}
+		Random rng = new Random(SimClock.getIntTime());
+		MapNode n = null;
+		do {
+			n = map.getNodes().get(rng.nextInt(map.getNodes().size()));
+		} while (n.isClosed());
+		n.close();
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + " [ ACCIDENT ] " + time + " ACCIDENT";
+	}
+}
---
+++ input/AccidentGenerator.java
@@ -0,0 +1,72 @@
+package input;
+
+import java.util.Random;
+
+import core.Settings;
+import core.SimClock;
+
+/**
+ * Danger message creation events generator. Creates danger message for every
+ * nodes of the simulation.
+ * 
+ * @author Virginie Collombon, David San
+ */
+public class AccidentGenerator implements EventQueue {
+	public static final String ACCIDENT_INTERVAL_S = "interval";
+	public static final String ACCIDENT_NROF_S = "nrofAccidents";
+	// public static final String PROBABILITY_OF_ACCIDENT = "accidentProb";
+	// private double accidentProb = 1;
+	private double nextEventsTime = 0;
+	/** Interval between accidents (min, max) */
+	private int[] accidentInterval;
+	/** Random number generator for this Class */
+	protected Random rng;
+	private int nrofAccidents;
+	private int count;
+
+	public AccidentGenerator(Settings s) {
+		// accidentProb = s.getDouble(PROBABILITY_OF_ACCIDENT);
+		this.rng = new Random(SimClock.getIntTime());
+		this.accidentInterval = s.getCsvInts(ACCIDENT_INTERVAL_S);
+		this.nrofAccidents = s.getInt(ACCIDENT_NROF_S);
+		this.count = 0;
+		if (this.accidentInterval.length == 1) {
+			this.accidentInterval = new int[] { this.accidentInterval[0],
+					this.accidentInterval[0] };
+		} else {
+			s.assertValidRange(this.accidentInterval, ACCIDENT_INTERVAL_S);
+		}
+		this.nextEventsTime = accidentInterval[0]
+				+ (accidentInterval[0] == accidentInterval[1] ? 0 : rng
+						.nextInt(accidentInterval[1] - accidentInterval[0]));
+	}
+
+	/**
+	 * Generates a (random) time difference between two events
+	 * 
+	 * @return the time difference
+	 */
+	protected int drawNextEventTimeDiff() {
+		int timeDiff = accidentInterval[0] == accidentInterval[1] ? 0 : rng
+				.nextInt(accidentInterval[1] - accidentInterval[0]);
+		return accidentInterval[0] + timeDiff;
+	}
+
+	@Override
+	public ExternalEvent nextEvent() {		
+		int interval = drawNextEventTimeDiff();
+		AccidentEvent ae = new AccidentEvent(this.nextEventsTime);
+		this.nextEventsTime += interval;
+		count++;
+		if(count > nrofAccidents){
+			this.nextEventsTime += Double.MAX_VALUE;
+		}
+		return ae;
+	}
+
+	@Override
+	public double nextEventsTime() {
+		return this.nextEventsTime;
+	}
+
+}

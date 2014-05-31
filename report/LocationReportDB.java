4501f573cc66abf96f4c7489b8680d0f1d23fe02
David San <davidsanfr@gmail.com>
2014-05-31 17:43:11 +0200

    Add LocationReportDB
    Location of all hosts of the simulation for every updates (heavy...)

---
+++ report/LocationReportDB.java
@@ -0,0 +1,70 @@
+package report;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+
+import core.DTNHost;
+import core.UpdateListener;
+import db.Queries;
+
+/**
+ * Location report stored in database
+ * 
+ * @author Virginie Collombon, David San
+ */
+public class LocationReportDB extends ReportDB implements UpdateListener {
+
+	protected PreparedStatement statement;
+
+	/**
+	 * Constructor.
+	 */
+	public LocationReportDB() {
+		super();
+	}
+
+	protected void initTable() {
+		try {
+			Statement s = connection.createStatement();
+			s.setQueryTimeout(30); // set timeout to 30 sec.
+			s.executeUpdate(Queries
+					.getQuery("LocationReportDB.drop_table_location"));
+			s.executeUpdate(Queries
+					.getQuery("LocationReportDB.create_table_location"));
+			s.close();
+			statement = connection.prepareStatement(Queries
+					.getQuery("LocationReportDB.insert_into_location"));
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void updated(List<DTNHost> hosts) {
+		Double time = getSimTime();
+		for (DTNHost host : hosts) {
+			try {
+				statement.setDouble(1, time);
+				statement.setInt(2, host.getAddress());
+				statement.setDouble(3, host.getLocation().getX());
+				statement.setDouble(4, host.getLocation().getY());
+				statement.executeUpdate();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	@Override
+	public void done() {
+		try {
+			statement.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		super.done();
+	}
+}

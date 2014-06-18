7327de0aab67f76d25fadaaf000bb481a49e6ed7
Virginie Collombon <virginie.collombon@gmail.com>
2014-06-18 20:59:07 +0200

    essai fix pour random walk

--- movement/RandomPathMapBasedMovement.java
+++ movement/RandomPathMapBasedMovement.java
@@ -6,6 +6,7 @@ import movement.map.DijkstraPathFinder;
 import movement.map.MapNode;
 import movement.map.PointsOfInterest;
 import core.Settings;
+import core.SimScenario;
 
 /**
  * 
@@ -58,10 +59,22 @@ public class RandomPathMapBasedMovement extends MapBasedMovement implements
 		assert nodePath.size() > 0 : "No path from " + lastMapNode + " to "
 				+ to + ". The simulation map isn't fully connected";
 
-		if (nodePath.size() < 1) {
-			if (getHost() != null) {
+		if (getHost() != null) {
+			if (SimScenario.getInstance().getMap()
+					.getNodeByCoord(getHost().getLocation()) != null
+					&& SimScenario.getInstance().getMap()
+							.getNodeByCoord(getHost().getLocation()).isClosed()) {
 				getHost().setStucked(true);
 			}
+		}
+		for (MapNode node : nodePath) {
+			if (node.isClosed()) {
+				if (getHost() != null) {
+					getHost().setStucked(true);
+				}
+			}
+		}
+		if (nodePath.size() < 1) {
 			return p;
 		}
 

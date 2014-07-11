53eba27c88d05cb457f3cee24459c287d3b6e6de
David San <davidsanfr@gmail.com>
2014-07-11 22:13:18 +0200

    Submit csv2db as job for OAR

--- bench/process-csv.sh
+++ bench/process-csv.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-# Convert db to csv
+# Convert CSV to SQLite for all reports
 
 usage="Usage : $0 <scenario_name> <results dir>"
 [[ $# -lt 2 ]] && echo $usage >&2 && exit 1
@@ -7,9 +7,14 @@ usage="Usage : $0 <scenario_name> <results dir>"
 scenario_name=$1
 result_dir=$2
 
-
 for csv in `ls $result_dir/$scenario_name_*/*.csv` ; do
-	./csv2db.sh $csv
+	db_file=`echo $csv | sed -e 's/csv$/db/'`
+	if [ ! -f $db_file ]; then
+		echo "Convert to SQLite $csv"
+		oarsub -l "core=1,walltime=72:0:0" --name "csv2db ${csv}" --stdout /dev/null --stderr /dev/null "./csv2db.sh $csv" &
+    	# For sequential run, comment previous line and
+    	# uncomment next
+		# ./csv2db.sh $csv
+	fi
 done
-
-#cd $OLDPWD
+echo "Jobs submitted."

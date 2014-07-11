44f1c51c1b082c795eecd2cf2d26f13657d949f4
David San <davidsanfr@gmail.com>
2014-07-11 18:32:09 +0200

    cleaner compilation

---
+++ bench/build-project.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+# OAR launcher for compiling the project
+
+usage="Usage : $0 <chemin the one>"
+[[ $# -lt 1 ]] && echo -e $usage >&2 && exit 1
+
+echo "Launch compilation job..."
+oarsub -l "core=1,walltime=0:30:00" -E /dev/null -O /dev/null "cd $1 && ant build-project"

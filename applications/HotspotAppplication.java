5d66d78f07c38b2a5f00a396475a257b330da099
David San <davidsanfr@gmail.com>
2014-07-16 14:20:11 +0200

    fix cfg

--- applications/HotspotAppplication.java
+++ applications/HotspotAppplication.java
@@ -16,67 +16,64 @@ import core.Settings;
  */
 
 public class HotspotAppplication extends Application {
-		/** Application ID */
-		public static final String APP_ID = "applications.HotspotApplication";
+	/** Application ID */
+	public static final String APP_ID = "applications.HotspotApplication";
 
-		/** Known Location Key Message */
-		public static final String KNOWN_LOCATIONS_KEY_MESSAGE = "KNOWN_LOCATIONS";
+	/** Known Location Key Message */
+	public static final String KNOWN_LOCATIONS_KEY_MESSAGE = "KNOWN_LOCATIONS";
 
+	/**
+	 * Creates a new hotspot application with the given settings.
+	 * 
+	 * @param s
+	 *            Settings to use for initializing the application.
+	 */
+	public HotspotAppplication(Settings s) {
+		super.setAppID(APP_ID);
+	}
 
-		/**
-		 * Creates a new hotspot application with the given settings.
-		 * 
-		 * @param s
-		 *            Settings to use for initializing the application.
-		 */
-		public HotspotAppplication(Settings s) {
-			super.setAppID(APP_ID);
-		}
+	/**
+	 * Copy-constructor
+	 * 
+	 * @param a
+	 */
+	public HotspotAppplication(HotspotAppplication a) {
+		super(a);
+	}
 
-		/**
-		 * Copy-constructor
-		 * 
-		 * @param a
-		 */
-		public HotspotAppplication(HotspotAppplication a) {
-			super(a);
-		}
-
-
-		/**
-		 * Handles an incoming message.
-		 * 
-		 * @param msg
-		 *            message received by the router
-		 * @param host
-		 *            host to which the application instance is attached
-		 */
-		@Override
-		public Message handle(Message msg, DTNHost host) {
-
-			/* Updating known locations map */
-			if (msg.getProperty(KNOWN_LOCATIONS_KEY_MESSAGE) != null) {
-				// System.err.println("[debug] node " + host.getAddress()
-				// + " receive 1 message with his known locations");
-				@SuppressWarnings("unchecked")
-				Map<DTNHost, Tuple<Coord, Integer>> knownLocationsMsg = (Map<DTNHost, Tuple<Coord, Integer>>) msg
-						.getProperty(KNOWN_LOCATIONS_KEY_MESSAGE);
-				// add map B element or update if already in map res
-				for (DTNHost hostMsg : knownLocationsMsg.keySet()) {
-					Coord c = knownLocationsMsg.get(hostMsg).getKey();
-					int stamp = knownLocationsMsg.get(hostMsg).getValue();
-					host.updateKnownLocations(hostMsg, c, stamp);
-				}
+	/**
+	 * Handles an incoming message.
+	 * 
+	 * @param msg
+	 *            message received by the router
+	 * @param host
+	 *            host to which the application instance is attached
+	 */
+	@Override
+	public Message handle(Message msg, DTNHost host) {
+		/* Updating known locations map */
+		if (msg.getProperty(KNOWN_LOCATIONS_KEY_MESSAGE) != null) {
+			// System.err.println("[debug] node " + host.getAddress()
+			// + " receive 1 message with his known locations");
+			@SuppressWarnings("unchecked")
+			Map<DTNHost, Tuple<Coord, Integer>> knownLocationsMsg = (Map<DTNHost, Tuple<Coord, Integer>>) msg
+					.getProperty(KNOWN_LOCATIONS_KEY_MESSAGE);
+			// add map B element or update if already in map res
+			for (DTNHost hostMsg : knownLocationsMsg.keySet()) {
+				Coord c = knownLocationsMsg.get(hostMsg).getKey();
+				int stamp = knownLocationsMsg.get(hostMsg).getValue();
+				host.updateKnownLocations(hostMsg, c, stamp);
 			}
-			return null;
 		}
+		return null;
+	}
 
-		@Override
-		public Application replicate() {
-			return new HotspotAppplication(this);
-		}
+	@Override
+	public Application replicate() {
+		return new HotspotAppplication(this);
+	}
 
-		@Override
-		public void update(DTNHost host) {
-		}
+	@Override
+	public void update(DTNHost host) {
+	}
 }
